ccdbb991b23cd5550095db33e6d34cd358bf3785
Patrick Chen <pc523@cornell.edu>
2022-04-30 12:33:29 -0400

    Updated dashboard again

--- src/index.js
+++ src/index.js
@@ -5,14 +5,16 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Dashboard from './dashboard';
+import Swap from './swap';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
       <Routes>
-        <Route path="/" element={<App />}/>
-        <Route path="/dashboard" element={<Dashboard />}/>
+        <Route path="/" element={<App />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/swap" element={<Swap />} />
       </Routes>
     </Router>
   </React.StrictMode>
---
+++ src/swap.js
@@ -0,0 +1,6 @@
+export default function Swap() {
+  return (
+    <div>
+    </div>
+  )
+}
