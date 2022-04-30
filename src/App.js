ec9322ab28c3753f0c42929611de86c4ad0de19f
Corfish123 <80220215+Corfish123@users.noreply.github.com>
2022-04-30 19:21:20 -0400

    update about us stuff

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ export function App() {
           <div className="mt-[10%] flex flex-col w-[50%]">
             <p className="text-9xl mt-auto mb-8 "
             >Stabler</p>
-            <p className="text-2xl mb-4">Disrupting the StableCoin Ecosyssystem</p>
+            <p className="text-2xl mb-4">Stabilizing the Crypto Ecosysystem</p>
             <p className="text-lg mb-8">Stabler is a over-collateralized, decentralized, and diversified stable coin that is deeply liquid, asset-backed, and used widely across Web3.</p>
             <Link to="/dashboard" className="mb-auto w-48 bg-indigo-400 hover:bg-indigo-600 py-2 px-4 text-center rounded transition ease-in-out delay-25 duration-250">
               Enter Dashboard
@@ -37,7 +37,7 @@ export function App() {
 
           <div className="px-10 py-20 w-[35rem] rounded-lg bg-indigo-100 shadow-lg">
             <h5 className="mb-2 font-bold text-4xl text-gray-900">Earn Interest</h5>
-            <p className="text-lg text-gray-700">Finally, to reach mass-adoption, Stable must achieve high utility. Stable is rapidly building new protocols and partnering with community initiatives to continue to expand the use cases for Stable.</p>
+            <p className="text-lg text-gray-700">Finally, to reach mass-adoption, we offer interest for simplying using STB as part of our revolutionary tokenomic model. </p>
           </div>
 
         </div>
