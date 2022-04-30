dcbdf39401ce63f893b9d77e938879ae4f56bbcb
Connor Chen <cjc353@cornell.edu>
2022-04-30 13:50:57 -0400

    changed tab name + nvbar title

--- src/navbar.js
+++ src/navbar.js
@@ -7,7 +7,7 @@ export default function Navbar() {
       <header className="w-screen">
         <div className="py-3 flex justify-between w-full bg-indigo-500">
           <div className="flex align-middle">
-            <Link className="transition linear delay-25 font-bold text-3xl text-white hover:text-black duration-250" to="/">Stabler Coin </Link>
+            <Link className="transition linear delay-25 font-bold text-3xl text-white hover:text-black duration-250" to="/">Stabler</Link>
           </div>
           <div className="flex align-middle">
             <button className="transition ease-in-out delay-25 border-white border-2 px-2 rounded-xl text-white hover:scale-105 hover:bg-white hover:text-black hover:border-black duration-250">Connect Wallet</button>
