8b27a409cd170136d273004b42894d2349277bdf
Eric Huang <erichuanglm@gmail.com>
2022-04-30 09:28:16 -0400

    poopy butthole

--- src/index.js
+++ src/index.js
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Dashboard from './dashboard';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />}/>
+        <Route path="/dashboard" element={<Dashboard />}/>
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
 
